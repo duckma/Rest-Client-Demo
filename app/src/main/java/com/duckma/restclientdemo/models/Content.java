c51ed71c87f48d44c10f2fd2ce79eabfe02ad434
Matteo Gazzurelli <info@duckma.com>
2016-01-12 10:09:25 +0100

    Added Content model

---
+++ app/src/main/java/com/duckma/restclientdemo/models/Content.java
@@ -0,0 +1,12 @@
+package com.duckma.restclientdemo.models;
+
+/**
+ * Copyright © 2016 DuckMa S.r.l. - http://duckma.com
+ * <p/>
+ * Created by Matteo Gazzurelli on 12/01/16.
+ */
+public class Content {
+    String id;
+    String title;
+    String image;
+}
